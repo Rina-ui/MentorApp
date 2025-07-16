a522a9a63dc98d40c2d85f1af343b3e16c5e4a46
Rina-ui <marinagracia895@gmail.com>
2025-07-16 19:39:46 +0200

    delayed second in homepage

--- lib/screen/Homepage.dart
+++ lib/screen/Homepage.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.push(
+          context, 
+          MaterialPageRoute(builder: (context) => Homepage())
+      );
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
